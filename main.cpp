80ec3a6fb8bd248ded16739931a3ff1883290fc2
nael <naelblog@gmail.com>
2022-05-21 12:42:03 +0200

    added cmake project with errors to test valgrind

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+class myClass
+{
+public:
+    myClass() = default;
+    void leak()
+    {
+        int * a = new int(5);
+    }
+    int uninit()
+    {
+        int * b;
+        return *b;
+    }
+};
+
+int main()
+{
+    myClass a;
+    a.leak();
+    int i=a.uninit();
+    (void)i;
+    std::cout << " hola" <<std::endl;
+    return 0;
+}
